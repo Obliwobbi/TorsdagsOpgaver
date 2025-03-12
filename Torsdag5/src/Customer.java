5c75a0d94cdc0976b1e3cc6050a7ab9276d43819
Obliwobbi <tobiashartzberg@gmail.com>
2025-03-12 14:28:46 +0100

    Update Customer.java

--- Torsdag5/src/Customer.java
+++ Torsdag5/src/Customer.java
@@ -7,23 +7,32 @@ public class Customer {
     static int counter;
 
     Customer (String firstName, String lastName, String userName){
-
         this.firstName = firstName;
         this.lastName = lastName;
         this.userName = userName;
 
         counter += 1;
         this.id = counter;
+    }
 
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLasttName(){
+        return lastName;
+    }
+    public void setLasttName(String lastName){
+        this.lastName = lastName;
     }
 
     @Override
     public String toString(){
-
         String s = "";
-
         return s;
-
     }
 
 }
